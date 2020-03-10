06c29ad84b0407da1c65be67198c38d4eaf528f2
Geofrey J. Mercado <48930924+geofreym21@users.noreply.github.com>
2020-03-10 15:35:50 -0400

    Update DataEng.sql

--- DataEng.sql
+++ DataEng.sql
@@ -1,6 +1,6 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
+-- Create tables
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR   NOT NULL,
